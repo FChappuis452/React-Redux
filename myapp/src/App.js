73de2f83db422d0c5ab83a109cc981fa95763aac
Fred C <fchappuis452@gmail.com>
2020-02-23 08:54:58 -0500

    Lesson 14 output LIst of item

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My first React App</h1>
-      <p>Welcome</p>
-      <Ninjas name="Ryu" age="25" belt="Black"/>
-      <Ninjas name="Yoshi" age="35" belt="Brown"/>
-    </div>
-  );
+class  App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React App</h1>
+        <p>Welcome</p>
+        <Ninjas ninjas={this.state.ninjas} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
