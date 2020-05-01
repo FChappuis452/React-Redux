c59bc517e4d81a5c38308f0021bc654053ce8bb9
Frederic Chappuis <fchappuis452@gmail.com>
2020-05-01 00:44:16 -0400

    Finished lesson 19

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -8,7 +8,7 @@ class  App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Yoshi', age: 21, belt: 'green', id: 2 },
       { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
     ]
   }
@@ -20,13 +20,20 @@ class  App extends Component {
       ninjas: ninjas
     })
   }
-
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id
+    });
+    this.setState({
+      ninjas: ninjas
+    })
+  }
   render() {
     return (
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas} />
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={this.state.ninjas} />
         <AddNinja addNinja={this.addNinja} />
       </div>
     );
